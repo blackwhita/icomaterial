71d3ce0e11315a2f3e238fc36a6196f3857b19cc
blackwhita <1709969037@qq.com>
2023-03-18 20:59:24 +0800

    Initial commit

---
+++ src/main/java/com/blackwhita/icomaterial/item/ItemBase.java
@@ -0,0 +1,10 @@
+package com.blackwhita.icomaterial.item;
+
+import com.blackwhita.icomaterial.IcoMaterial;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ItemBase {
+    public static final DeferredRegister<Item>ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, IcoMaterial.MODID);
+}
