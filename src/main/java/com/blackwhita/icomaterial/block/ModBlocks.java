233de9769d91564cfc37acc0d667e869cb4beafe
blackwhita <1709969037@qq.com>
2023-03-19 17:54:38 +0800

    11

--- src/main/java/com/blackwhita/icomaterial/block/ModBlocks.java
+++ src/main/java/com/blackwhita/icomaterial/block/ModBlocks.java
@@ -1,4 +1,22 @@
 package com.blackwhita.icomaterial.block;
 
+import com.blackwhita.icomaterial.IcoMaterial;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, IcoMaterial.MODID);
+
+    public static final RegistryObject<Block> BLOCK_DEMO = BLOCKS.register("block_demo", () -> new Block(getDefaultProperties()));
+
+    public static BlockBehaviour.Properties getDefaultProperties() {
+        return BlockBehaviour.Properties.of(Material.STONE)
+                .requiresCorrectToolForDrops()
+                .strength(3.0F);
+    }
+
 }
--- src/main/java/com/blackwhita/icomaterial/item/ModItems.java
+++ src/main/java/com/blackwhita/icomaterial/item/ModItems.java
@@ -1,9 +1,32 @@
 package com.blackwhita.icomaterial.item;
 
 import com.blackwhita.icomaterial.IcoMaterial;
+import com.blackwhita.icomaterial.block.ModBlocks;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.NotNull;
 
 public class ModItems {
-    public static final DeferredRegister<net.minecraft.world.item.Item>ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, IcoMaterial.MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, IcoMaterial.MODID);
+
+    public static Item.Properties getDefaultProperties() {
+        return new Item.Properties().tab(ModTab);
+    }    public static final RegistryObject<Item> ITEM_DEMO = ITEMS.register("item_demo", () -> new Item(getDefaultProperties()));
+
+    public static final RegistryObject<Item> BLOCK_DEMO = ITEMS.register("block_demo", () -> new BlockItem(ModBlocks.BLOCK_DEMO.get(), getDefaultProperties()));
+
+    public static CreativeModeTab ModTab = new CreativeModeTab(IcoMaterial.MODID) {
+        @Override
+        @NotNull
+        public ItemStack makeIcon() {
+            return new ItemStack(ITEM_DEMO.get());
+        }
+    };
+
+
 }
